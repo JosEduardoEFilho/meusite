94b794989613dfd44744bdb52c73a17755ba51be
JosEduardoEFilho <86692775+JosEduardoEFilho@users.noreply.github.com>
2021-06-29 19:58:28 -0300

    Montonaro Cars

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Página Principal</title>
+    </head>
+    
+    <link href="bootstrap-4.5.2-dist/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+    <script src="bootstrap-4.5.2-dist/js/jquery-3.5.1.min.js" type="text/javascript"></script>
+    <script src="bootstrap-4.5.2-dist/js/bootstrap.js" type="text/javascript"></script>
+    
+    <body background="https://s1.1zoom.me/b5050/540/BUGATTI_2019_La_Voiture_Noire_Side_Black_Black_568141_3840x2160.jpg">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="index.php"> <font type="Arquicoco">Montonaro Cars</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+          <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
+          Cadastros
+        </a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="cad_marcas.php"><font color="white">Cadastro de Marcas</font></a>
+            <a class="dropdown-item" href="cad_ano.php"><font color="white">Cadastro de Anos</font></a>
+            <a class="dropdown-item" href="cad_cores.php"><font color="white">Cadastro de Cores</font></a>
+            <a class="dropdown-item" href="cad_documentos.php"><font color="white">Cadastro de Documentos</font></a>
+            <a class="dropdown-item" href="cad_localizacao.php"><font color="white">Cadastro de Localização</font></a>
+            <a class="dropdown-item" href="cad_veiculos.php"><font color="white">Cadastro de Veículos</font></a>
+            <a class="dropdown-item" href="cad_vendas.php"><font color="white">Cadastro de Vendas</font></a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Consultas
+        </a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="consulta01.php"><font color="white">Consultar Marcas</font></a>
+            <a class="dropdown-item" href="consultaAnos.php"><font color="white">Consultar Anos</font></a>
+            <a class="dropdown-item" href="consultaCores.php"><font color="white">Consultar Cores</font></a>
+            <a class="dropdown-item" href="consultaDocumentos.php"><font color="white">Consultar Documentos</font></a>
+            <a class="dropdown-item" href="consultaLocalizacoes.php"><font color="white">Consultar Localização</font></a>
+            <a class="dropdown-item" href="consultaveic.php"><font color="white">Consultar Veículos</font></a>
+            <a class="dropdown-item" href="consultaVendas.php"><font color="white">Consultar Vendas</font></a>
+        </div>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+        <br/>
+        <div class="container">
+            <div class="jumbotron jumbotron-fluid">
+                <div class="container">
+        <font>
+        <p><h1 class="display-3"><CENTER>Montonaro Cars</center></h1>
+        <center><p class="lead">Olá, bem vindo a <b>MONTONARO CARS!</b>
+            Aqui você poderá realizar o cadastro de carros que chegam em sua agência
+           e logo após realizar uma consulta dos carros que você possui cadastrado.</center>
+        <hr class="my-4">
+        <P><h2 class="display-4"><center>ETEC EURO ALBINO DE SOUZA</center></h2>
+        <center><p class="lead"> A <b>ETEC Euro Albino de Souza</b> é uma escola que nos ajudou no desenvolvimento dessa plataforma.
+            A escola realizou este trabalho juntamente com seu alunos como método de prática escolar.
+            Caso queira saber mais sobre a escola clique no botão abaixo:<br/>
+            
+            <br/>
+            <p><a class="btn btn-info btn-lg" href="http://www.etecmogiguacu.com.br/v2/">Saiba mais!</a><br/>
+                </div>
+                </div>
+                
+        </div>
+        
+        <br>
+        
+    </body>
+</html>
